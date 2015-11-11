c23af52a2aa9bca9cde5341c916df0124722598d
wyqzl <wuyuqian19950217@qq.com>
2015-11-11 20:27:38 +0800

    printf
    多加一行

--- text/main.cpp
+++ text/main.cpp
@@ -18,6 +18,8 @@ int main(int argc, const char * argv[]) {
     a[3]=55;
     p=a;
     printf("&a[0]=%x &a[1]=%x &a[2]=%x &a[3]=%x a=%x *p=%x",&a[0],&a[1],&a[2],&a[3],a,*p);
+    printf("&a[0]=%x &a[1]=%x &a[2]=%x &a[3]=%x a=%x *p=%x",&a[0],&a[1],&a[2],&a[3],a,*p);
+
     
     
     
