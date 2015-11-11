e159a2613cb29c09231eaf12482abefe9da9bedd
wyqzl <wuyuqian19950217@qq.com>
2015-11-11 19:10:09 +0800

    第一次提交

--- text/main.cpp
+++ text/main.cpp
@@ -10,6 +10,17 @@
 
 int main(int argc, const char * argv[]) {
     // insert code here...
-    std::cout << "Hello, World!\n";
+    int a[4];
+    int *p;
+    a[0]=22;
+    a[1]=33;
+    a[2]=44;
+    a[3]=55;
+    p=a;
+    printf("&a[0]=%x &a[1]=%x &a[2]=%x &a[3]=%x a=%x *p=%x",&a[0],&a[1],&a[2],&a[3],a,*p);
+    
+    
+    
+    
     return 0;
 }
